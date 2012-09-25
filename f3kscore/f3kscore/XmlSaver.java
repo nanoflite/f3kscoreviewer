53effd697f24c411e19a755c1219ee431c524c6f
Johan Van den Brande <johan@vandedenbrande.com>
2012-09-25 14:00:53 +0200

    Fix for XmlSaver, data contained no flight times.

--- f3kscore/f3kscore/XmlSaver.java
+++ f3kscore/f3kscore/XmlSaver.java
@@ -2,49 +2,68 @@ package f3kscore;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 import com.thoughtworks.xstream.XStream;
 
 public class XmlSaver {
+	
+	private String fileName = "";
+	private Competition competition = null;
+	private F3KScore f3kscore;
 	
 	public static void main(String[] args) {
 		if (args.length != 1) {
 			System.out.println("program <in>");
 			System.exit(1);
 		}
-		System.out.println("Saver...");
-		try {
-			System.out.println("Loading: " + args[0]);
-			F3KScore f3kscore = new F3KScore(args[0]);
+		XmlSaver xmlSaver = new XmlSaver(args[0]);
+		xmlSaver.run();
+	}
+	
+	public XmlSaver(String fileName) {
+		this.fileName = fileName;
+	}
+	
+	public void run() {
+		try {			
+			f3kscore = new F3KScore(null);
+
+			Method methodLoad = f3kscore.getClass().getDeclaredMethod("loadFromFile", File.class);
+			methodLoad.setAccessible(true);
+			File file = new File(fileName);
+			methodLoad.invoke(f3kscore, file);
 
 			Field field = f3kscore.getClass().getDeclaredField("competition");
-			field.setAccessible(true);
-			Competition competition = (Competition) field.get(f3kscore);
+			field.setAccessible(true);			
+			this.competition = (Competition) field.get(f3kscore);
 			
-			save(args[0] + ".xml", competition);
+			Method methodCalcResultList = this.competition.getClass().getDeclaredMethod("calcResultList");
+			methodCalcResultList.setAccessible(true);
+			methodCalcResultList.invoke(this.competition);
+			
+			save();
+	
+			System.exit(0);
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
-		System.exit(0);
-		
 	}
 	
-	private static void save(String fileName, Competition competition)
+	private void save()
 	{
-		File file = new File(fileName);
-		
 		try {
+			File file = new File(this.fileName + ".xml");
 			XStream xstream = new XStream();
 			FileOutputStream fs = new FileOutputStream(file);
 			ObjectOutputStream oos = xstream.createObjectOutputStream(fs);
-			oos.writeObject(competition);
+			oos.writeObject(this.competition);
 			oos.close();
-		}
-		catch (IOException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}	
 		
